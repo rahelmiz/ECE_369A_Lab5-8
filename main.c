13737bb9ef463a6de6b8acb2e19b244160fa9e8c
rahelmiz <rahlboo11@gmail.com>
2021-09-19 15:20:10 -0700

    Update main.c
    functionality includes mapping from a 1D array back to a 2D array

--- main.c
+++ main.c
@@ -1,11 +1,42 @@
 
 #include <stdio.h>
+
+int return1, return2;
+struct pos_2d{
+    int row;
+    int col;
+};
+
 int position_1D(int x,int y,int width){
     //x: row number in the 2d array
     //y: column number in the 2d array
     //width: number of columns in the 2d array
-    int position_1D = (x-1)*width + y;
+    int position_1D = (x)*width + y;
     return position_1D;
+}
+
+int get_row(int pos_1d, int width){
+//return the row # of the 2D array,
+//given the mapped 1D position, and width of the 2D array
+    unsigned int res = pos_1d;
+    unsigned int count = 0;
+    while (!(res < width)){
+        res = res - width;
+        //printf("%d ",res);
+        count +=1;
+    }
+    return count;
+}
+
+int get_col(int pos_1d, int width){
+//return the column # of the 2D array,
+//given the mapped 1D position, and width of the 2D array
+    unsigned int res = pos_1d;
+    while (!(res < width)){
+        res = res - width;
+        //printf("%d ",res);
+    }
+    return res;
 }
 
 void populate_array(int rows, int cols, int array[rows][cols]){
@@ -34,7 +65,14 @@ int main(int argc, const char * argv[]) {
 
     //populate_array(rows, cols, arr);
     //print_array(rows, cols, arr);
-    int pos1D = position_1D(4,1, rows);
-    printf("%d ", pos1D);
+    //int pos1D = position_1D(4,1, rows);
+    
+    int row = get_row(21, 8);
+    int col = get_col(21, 8);
+    printf("row: ");
+    printf("%d\n", row);
+    
+    printf("col : ");
+    printf("%d\n", col);
     return 0;
 }
