92692be256bf12856394e48b2491c4fbb0e97b97
rahelmiz <rahlboo11@gmail.com>
2021-09-22 07:56:18 -0700

    Update array_dim_converter.s

--- array_dim_converter.s
+++ array_dim_converter.s
@@ -1,6 +1,4 @@
-
-
-po$s1D:
+pos1D:
  # &a0: address of the array containing the dimension information
  # a0[1] = width = number of cols in frame
  # a1 = row = row number in 2D array
@@ -11,7 +9,7 @@ po$s1D:
     add $v0, $s1, $a2 # v0 = (row * width) + col
     jr $ra
 
-po$s2D:
+pos2D:
 # pos2D functionality:
 # given position in 1D array, and width of orig 2D array, map the
 # 1D location back to row, col in the 2D array.
@@ -22,7 +20,6 @@ po$s2D:
 # $v0: row idx of elem in 2-D array$
 # $v1: col idx of elem in 2-D array$
 
-pos2d:
 addi $s0, $zero, $zero # s0 = count = 0
 addi $s1, $a1, $zero #s1 = pos1D = res
 lw $s2, 4($a0) # s2 = width
@@ -33,7 +30,4 @@ lw $s2, 4($a0) # s2 = width
     bneq $t0, $zero, loop # loop until res < width
 addi $v0, $s0, $zero #v0 = count; gives the row number
 addi $v1, $s1, $zero #v1 = res; gives the col number
-jr $ras
-
-
-
+jr $ra
